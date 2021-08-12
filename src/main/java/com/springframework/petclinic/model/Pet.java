1f1431d35d9084982f822b94583a74ed47c5a684
NursultanCode <55210164+NursultanCode@users.noreply.github.com>
2021-08-12 14:59:35 +0600

    Creating BaseEntity. Closes #10

--- src/main/java/com/springframework/petclinic/model/Pet.java
+++ src/main/java/com/springframework/petclinic/model/Pet.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDateTime birthDate;
--- src/main/java/com/springframework/petclinic/model/PetType.java
+++ src/main/java/com/springframework/petclinic/model/PetType.java
@@ -3,6 +3,6 @@ package com.springframework.petclinic.model;
 import lombok.Data;
 
 @Data
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 }
