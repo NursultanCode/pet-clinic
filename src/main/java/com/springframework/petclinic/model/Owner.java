9af88955642b33258290b7cdc17127a6d272c494
NursultanCode <55210164+NursultanCode@users.noreply.github.com>
2021-08-13 11:02:55 +0600

    adding info for Owner

--- src/main/java/com/springframework/petclinic/model/Owner.java
+++ src/main/java/com/springframework/petclinic/model/Owner.java
@@ -9,6 +9,9 @@ import java.util.Set;
 @Data
 @Entity
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String telephone;
 
     @OneToMany
     private Set<Pet> pets;
